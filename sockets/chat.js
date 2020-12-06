45ce02e29b26728cb8a1fb8b5ffbd9b6311ab938
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro-2.local>
2020-12-06 13:51:05 -0800

    Users can create new channels and join

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,10 +1,8 @@
 //chat.js
-module.exports = (io, socket, onlineUsers) => {
+//Make sure to add channels to module.exports parameters
+module.exports = (io, socket, onlineUsers, channels) => {
 
-  socket.on('get online users', () => {
-  //Send over the onlineUsers
-  socket.emit('get online users', onlineUsers);
-  })
+  socket.on('new user', (username) => {
     //Save the username as key to access the user's socket id
     onlineUsers[username] = socket.id;
     //Save the username to socket as well. This is important for later.
@@ -12,6 +10,12 @@ module.exports = (io, socket, onlineUsers) => {
     console.log(`✋ ${username} has joined the chat! ✋`);
     io.emit("new user", username);
   })
+
+  socket.on('new message', (data) => {
+    console.log(`🎤 ${data.sender}: ${data.message} 🎤`)
+    io.emit('new message', data);
+  })
+
 
   socket.on('new message', (data) => {
     console.log(`🎤 ${data.sender}: ${data.message} 🎤`)
@@ -27,6 +31,16 @@ module.exports = (io, socket, onlineUsers) => {
   });
 
   socket.on('new channel', (newChannel) => {
-        console.log(newChannel);
-    })
+    //Save the new channel to our channels object. The array will hold the messages.
+    channels[newChannel] = [];
+    //Have the socket join the new channel room.
+    socket.join(newChannel);
+    //Inform all clients of the new channel.
+    io.emit('new channel', newChannel);
+    //Emit to the client that made the new channel, to change their channel to the one they made.
+    socket.emit('user changed channel', {
+      channel : newChannel,
+      messages : channels[newChannel]
+    });
+  })
 }
